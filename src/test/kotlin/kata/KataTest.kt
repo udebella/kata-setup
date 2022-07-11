1b934a7150649aa0ee56c8f0393cabad443e5ab8
Ubu <ubald2b@gmail.com>
2022-07-11 17:15:04 +0200

    Setup for kotlin

---
+++ src/test/kotlin/kata/KataTest.kt
@@ -0,0 +1,11 @@
+package kata
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class KataTest {
+    @Test
+    fun works() {
+        assertThat(true).isFalse()
+    }
+}
