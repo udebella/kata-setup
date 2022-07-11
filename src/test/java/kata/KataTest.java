92bfd8581157a1384ddecaf9c4caa282321b6950
Ubu <ubald2b@gmail.com>
2022-07-11 17:15:04 +0200

    Setup for java

---
+++ src/test/java/kata/KataTest.java
@@ -0,0 +1,12 @@
+package kata;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class KataTest {
+    @Test
+    public void works() {
+        assertThat(true).isFalse();
+    }
+}
