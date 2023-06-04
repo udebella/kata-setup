0f1e7142cf82bf3b96933dd3f94c5d9d0757b452
Ubu <ubald2b@gmail.com>
2023-06-05 00:43:42 +0200

    Setup for deno

---
+++ src/deps.ts
@@ -0,0 +1,2 @@
+export { describe, it } from "https://deno.land/std@0.190.0/testing/bdd.ts";
+export { expect, mock } from "https://deno.land/x/expect@v0.3.0/mod.ts";
---
+++ src/kata.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, it } from "./deps.ts";
+
+describe("some tests", () => {
+  it("works", () => {
+    expect(3).toBe(3);
+  });
+});
